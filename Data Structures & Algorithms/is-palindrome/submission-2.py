175b984db8d3492bd29f9d024704d816d9cd5109
JacobP <jaco.pala@icloud.com>
2026-07-08 01:22:17 -0500

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,12 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=re.sub(r'[^A-Za-z0-9]', '',s).lower()
+        print(s)
+        l,r=0,len(s)-1
+        while l<r:
+            if s[l]!=s[r]:
+                return False
+            l+=1
+            r-=1
+        return True
